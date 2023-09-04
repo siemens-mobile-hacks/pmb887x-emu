c5348ed8e1f16a4d9affa7d714db0ce46e294449
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2023-09-04 05:03:00 +0300

    new boards config system

--- tools/make_dist_win.sh
+++ tools/make_dist_win.sh
@@ -6,9 +6,12 @@ SRC=$(realpath $(dirname "$(realpath $0)")/..)
 DIST_DIR="$SRC/build/dist-win"
 BUILD_DIR="$SRC/build/qemu-win"
 LIBS_DIR="$SRC/build/libs-win"
+BOARDS_DIR="$SRC/bsp/lib/data/board"
 
 rm -rf "$DIST_DIR"
 mkdir -p "$DIST_DIR/bin"
 
 find "$LIBS_DIR" -iname '*.dll' -exec cp -v {} "$DIST_DIR/bin" \;
 cp -v "$BUILD_DIR/qemu-system-arm.exe" "$DIST_DIR/bin"
+rsync -av --delete "$BOARDS_DIR/" "$DIST_DIR/boards/"
+cp -v "$SRC/emu" "$DIST_DIR/emu"
