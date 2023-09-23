9a7100abf690811acb30df09a4d16c2fc6a2d9a5
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2023-09-23 06:01:37 +0300

    fix win dist

--- tools/make_dist_win.sh
+++ tools/make_dist_win.sh
@@ -7,9 +7,13 @@ DIST_DIR="$SRC/build/dist-win"
 BUILD_DIR="$SRC/build/qemu-win"
 LIBS_DIR="$SRC/build/libs-win"
 BOARDS_DIR="$SRC/bsp/lib/data/board"
+DIST_ZIP="/tmp/pmb887x-emu-windows.zip"
 
 rm -rf "$DIST_DIR"
 mkdir -p "$DIST_DIR/bin"
+
+find /usr/x86_64-w64-mingw32/lib/ -iname '*.dll' -exec cp -v {} "$DIST_DIR/bin" \;
+find /usr/lib/gcc/x86_64-w64-mingw32/12-win32 -iname '*.dll' -exec cp -v {} "$DIST_DIR/bin" \;
 
 find "$LIBS_DIR" -iname '*.dll' -exec cp -v {} "$DIST_DIR/bin" \;
 cp -v "$BUILD_DIR/qemu-system-arm.exe" "$DIST_DIR/bin"
@@ -17,5 +21,5 @@ rsync -av --delete "$BOARDS_DIR/" "$DIST_DIR/boards/"
 cp -v "$SRC/emu" "$DIST_DIR/emu"
 cd "$DIST_DIR"
 
-[[ -f /tmp/pmb887x-emu.zip ]] && rm /tmp/pmb887x-emu.zip
-zip /tmp/pmb887x-emu.zip -r .
+[[ -f "$DIST_ZIP" ]] && rm "$DIST_ZIP"
+zip "$DIST_ZIP" -r .
