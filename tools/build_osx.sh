151970311facb9b1fab9417dcad24939e832924d
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2023-09-23 17:45:00 +0300

    test osx

---
+++ tools/build_osx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+SRC=$(realpath $(dirname "$(realpath $0)")/..)
+
+BUILD_DIR="$SRC/build/qemu"
+
+[[ -d "$BUILD_DIR" ]] || mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+[[ "$1" == "configure" ]] && rm -f "$BUILD_DIR/.configured"
+
+[[ -e "$BUILD_DIR/.configured" ]] || {
+	"$SRC/qemu/configure" \
+		--target-list=arm-softmmu \
+		--disable-werror \
+		--disable-install-blobs \
+		--enable-lto \
+		--enable-strip
+	touch "$BUILD_DIR/.configured"
+}
+
+make -j$((`nproc` + 1))
